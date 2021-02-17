46a54e5261883e113de2524201163b2eed4f3c7a
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2021-02-16 23:29:44 -0500

    Feat: callbacks con API Rick And Morty, creando script en package.json

---
+++ src/callback/challenge.js
@@ -0,0 +1,42 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+const API = 'https://rickandmortyapi.com/api/character/';
+
+
+/**
+ * 
+ * @param {*} url_api 
+ * @param {*} callback 
+ */
+function fetchData(url_api, callback){
+    let xhttp = new XMLHttpRequest();
+    //param  método, url, asincrono = true
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = function(event) {
+        if(xhttp.readyState === 4){
+         if(xhttp.status === 200) {
+            callback(null, JSON.parse(xhttp.responseText)) //parseamos a un string
+         }else{
+            const error = new Error('Error' + url_api); //Error clase implicita js
+            return callback(error, null)
+         }   
+        }
+    }
+    xhttp.send();
+}
+
+
+
+//le pasamos la funcion como parametro 
+fetchData(API, function(error1, data1) { 
+    if(error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2) {
+        if(error2) return console.error(error2);
+            fetchData(data2.origin.url, function(error3, data3) {
+                if(error3) return console.error(error3);
+                console.log(data1.info.count);
+                console.log(data2.name);
+                console.log(data3.dimension);
+              })
+    })
+});
